e5ea9fd4ffb0d1d1c0c1d243e8aeb6ce90c54748
Taha <kyojin.rar@gmail.com>
2024-10-29 00:12:18 +0100

    working on the design added the sections for todo doing and done, working on the styles of the cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
-const { addDynamicIconSelectors } = require('@iconify/tailwind');
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    colors: {
-      whiteone: "#CED4DA",
-      whitetwo: "#DEE2E6",
-      whitethree: "#E9ECEF",
-      greyone: "#495057",
-      greytwo: "#343A40",
-      redone: "#E8604E",
-      greenone: "#6DD483",
-      greentwo: "#4A9A77",
-      yellowone: "#DEE178",
-      yellowtwo: "#E0E06D",
-      blueone: "#73CDE1",
-    },
-    fontFamily: {
-      brico: "Bricolage Grotesque",
+    extend: {
+      colors: {
+        whiteone: "#CED4DA",
+        whitetwo: "#DEE2E6",
+        whitethree: "#E9ECEF",
+        greyone: "#495057",
+        greytwo: "#343A40",
+        greenone: "#6DD483",
+        greentwo: "#4A9A77",
+        yellowone: "#DEE178",
+        yellowtwo: "#E0E06D",
+        blueone: "#73CDE1",
+        datecol: "#B4BFCF",
+        cardred: "#FF8080",
+        cardredhov: "#FC8D8D",
+        cardredactive: "#DD6F6F",
+      },
+      fontFamily: {
+        brico: "Bricolage Grotesque",
+      },
     },
   },
-  plugins: [
-      addDynamicIconSelectors(),
-  ],
+  plugins: [addDynamicIconSelectors()],
 };
